4348f41940faace45c1b1854816463a92c185a08
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2024-03-10 17:37:05 -0300

    Created model Veiculo and updated Principal

--- src/main/java/br/com/alura/TabelaFip/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFip/principal/Principal.java
@@ -2,11 +2,15 @@ package br.com.alura.TabelaFip.principal;
 
 import br.com.alura.TabelaFip.model.Dados;
 import br.com.alura.TabelaFip.model.Modelos;
+import br.com.alura.TabelaFip.model.Veiculo;
 import br.com.alura.TabelaFip.service.ConsumoApi;
 import br.com.alura.TabelaFip.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -28,6 +32,7 @@ public class Principal {
         var opcao = leitura.nextLine();
         String endereco;
 
+        //Seleciona o tipo de veículo desejado
         if (opcao.toLowerCase().contains("carr")){
             endereco = URL_BASE + "carros/marcas";
         } else if (opcao.toLowerCase().contains("mot")) {
@@ -43,6 +48,7 @@ public class Principal {
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
 
+        //Procura a marca do veículo
         System.out.println("Informe o código da marca para consulta: ");
         var codigoMarca = leitura.nextLine();
 
@@ -54,5 +60,33 @@ public class Principal {
         modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        //Filtra o modelo do carro desejado
+        System.out.println("\nDigite um trecho do nome do carro a ser buscado: ");
+        var nomeVeiculo = leitura.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+        System.out.println("\nModelos filtrados: ");
+        modelosFiltrados.forEach(System.out::println);
+
+        //Busca todos os carros disponiveis por modelo
+        System.out.println("\nDigite o código do modelo para buscar os valores de avaliação: ");
+        var codigoModelo = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumo.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumo.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+        System.out.println("\nTodos os veículos filtrados com avaliações por ano: ");
+        veiculos.forEach(System.out::println);
     }
 }
